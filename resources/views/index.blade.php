8ed6af914a8373bf2be0b42ca863c31ff625af0f
lifeo <lifeonobl1vion@gmail.com>
2024-11-01 13:20:40 +0300

    v2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,25 @@
 <body>
 <div>
     <p>Hello</p>
+    <div>
+        <h1>Добавить животное</h1>
+        <form action="" method="POST">
+            @csrf
+            <div class="input">
+                <label for="nickname">Кличка</label>
+                <input type="text" placeholder="Введите кличку..." name="nickname"  required autofocus>
+            </div>
+            <div class="input">
+                <label for="age">Возраст</label>
+                <input type="number" placeholder="Введите возраст..." name="age" required>
+            </div>
+            <div class="input">
+                <label for="breed">Порода</label>
+                <input type="text" placeholder="Введите породу..." name="breed" required>
+            </div>
+            <button type="submit">Добавить</button>
+        </form>
+    </div>
 </div>
 
 </body>
